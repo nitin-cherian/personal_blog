7b03ecbcadb1c9ee0e952379ed86e1ff3a2bcaa6
nitin-cherian <nitin.cherian@gmail.com>
2018-03-25 15:33:02 +0530

    Add web form for user login.

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,10 @@ __init__.py:  Exposes names that will be globally available to the application.
 
 from flask import Flask
 
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,10 @@
 routes.py - All the view functions are defined here.
 
 """
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -43,3 +44,11 @@ def home():
 
     return render_template('home.html', title='Home', posts=posts)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for {}, remember_me = {}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
